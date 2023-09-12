8ec47a0065b94a66816b0eebee55e819ff22ab88
tlqkrus012345 <tlqkrus012345@naver.com>
2023-09-12 21:33:35 +0900

    nginx-docker

---
+++ src/main/java/com/example/nginxdockerproject/NginxDockerProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.nginxdockerproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NginxDockerProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NginxDockerProjectApplication.class, args);
+    }
+
+}
